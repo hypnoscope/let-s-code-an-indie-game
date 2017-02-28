b163d77911bfe8b93e5238da0aff3ff7306e92fd
g <gareth.rogers0@gmail.com>
2017-02-28 20:48:51 +1100

    Episode 19 start

---
+++ episode_19/README.md
@@ -0,0 +1,5 @@
+#Let's Code and Indie Game - Episode 18 - Overlaps
+
+##What's Covered
+
+In this episode we use the bounding boxes to checkto see if entities are colliding.
---
+++ episode_19/src/logic/rooms/room.lua
@@ -0,0 +1,67 @@
+local vector = require("src.math.vector")
+local tilesheet = require("src.graphics.tilesheet")
+local tilemap = require("src.logic.rooms.tilemap")
+
+local room = {}
+
+local walkable = function (self, x, z)
+  local screenPos = vector.worldToScreen({x=x, z=z, y=0})
+  if screenPos.x < 0 or screenPos.y < 0 then return false end
+  local tileChar = self.tilemap:getTile(screenPos.x, screenPos.y, self.tilesheet.tileSize)
+  return (tileChar == ',' or tileChar == ".")
+end
+
+local draw = function (self, view)
+  self.tilemap:draw(view, self.tilesheet)
+
+  for _, entity in ipairs(self.entities) do
+    entity:draw(view)
+  end
+end
+
+local update = function (self, game, map)
+  for _, entity in ipairs(self.entities) do
+    entity:update(game)
+
+    game.player:collisionCheck(entity)
+    entity:collisionCheck(game.player)
+    for _, anotherEntity in ipairs(self.entities) do
+      entity:collisionCheck(anotherEntity)
+    end
+  end
+
+  if game.player.drawX > self.roomWidth - self.tilesheet.tileSize then
+    map:nextRoom(game)
+  end
+
+  if game.player.drawX < self.tilesheet.tileSize then
+    map:previousRoom(game)
+  end
+end
+
+room.create = function (entities)
+  local inst = {}
+
+  inst.tilesheet = tilesheet.create("assets/sprites/tiles/dungeon.png", 8)
+  inst.tilemap = tilemap.create()
+
+  inst.color = {
+    math.random(255), math.random(255), math.random(255)
+  }
+
+  inst.entities = entities
+  inst.roomWidth = 50 * inst.tilesheet.tileSize
+  inst.roomHeight = 22 * inst.tilesheet.tileSize
+  inst.entranceX = -80
+  inst.entranceZ = 140
+  inst.exitX = 275
+  inst.exitZ = 140
+
+  inst.draw = draw
+  inst.update = update
+  inst.walkable = walkable
+
+  return inst
+end
+
+return room
